fcaff5bc06a4d1914c7998807ea5c2b484d21dc0
le2tang <le2tang@uwaterloo.ca>
2023-12-31 15:16:17 -0500

    Add ability for users to see carts

--- routes/pages.js
+++ routes/pages.js
@@ -209,4 +209,27 @@ router.post("/admin/carts/:username", async function (req, res) {
   }
 })
 
+router.get("/user/:username", async function (req, res) {
+  if (!req.body) {
+    res.status(400).send({ message: "Invalid request" })
+  }
+
+  carts_model.getCartsByUsername(req.params.username).then(
+    function (carts) {
+      carts.forEach(function (cart) {
+        cart.address = null
+        cart.arrival = null
+        cart.contact_method = null
+        cart.contact_address = null
+        cart.status = null
+      })
+      res.render("user_carts_view", { paths: app_config.paths, carts: carts })
+    }
+  ).catch(
+    function (err) {
+      res.status(500).send({ message: err })
+    }
+  )
+})
+
 module.exports = router
